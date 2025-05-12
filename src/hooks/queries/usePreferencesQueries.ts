2760d08ec535cdf5c93f410388cf55950d9bc806
Arlind Bekjiri <arlind.bekiri@icloud.com>
2025-05-12 15:10:20 +0200

    feature(settings): implement username with Capacitor Preferences
    - format fix

--- src/hooks/queries/usePreferencesQueries.ts
+++ src/hooks/queries/usePreferencesQueries.ts
@@ -1,7 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { PreferencesService } from '@/services';
 
-
 export const preferencesKeys = {
   all: ['preferences'] as const,
   userName: () => [...preferencesKeys.all, 'userName'] as const,
