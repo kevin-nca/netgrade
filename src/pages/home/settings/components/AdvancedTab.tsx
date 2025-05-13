9d638f535dd6bac179c5c90c76c579e11639c444
Gabrielelogiurato <151849343+Gabrielelogiurato@users.noreply.github.com>
2025-05-13 15:50:04 +0200

    Update AdvancedTab.tsx

--- src/pages/home/settings/components/AdvancedTab.tsx
+++ src/pages/home/settings/components/AdvancedTab.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { trashOutline } from 'ionicons/icons';
-import { IonText } from '@ionic/react';
+import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import SettingsHeader from './SettingsHeader';
 import SettingsGroup from './SettingsGroup';
 import SettingsItem from './SettingsItem';
-
 interface AdvancedTabProps {
   onReset: () => void;
 }
 
 const AdvancedTab: React.FC<AdvancedTabProps> = ({ onReset }) => {
   return (
-    <>
-      <SettingsHeader
-        title="Erweiterte Einstellungen"
-        subtitle="Daten verwalten und exportieren"
-      />
-      <SettingsGroup>
-        <IonText color="medium" className="ion-padding-horizontal">
-          <p>Hier können Sie Ihre Daten exportieren oder zurücksetzen.</p>
-        </IonText>
-        <SettingsItem
-          icon={trashOutline}
-          label="Alle Daten zurücksetzen"
-          onClick={onReset}
-          color="danger"
-        />
-      </SettingsGroup>
-    </>
+    <IonGrid>
+      <IonRow>
+        <IonCol>
+          <SettingsHeader
+            title="Erweiterte Einstellungen"
+            subtitle="Daten zurücksetzen"
+          />
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol>
+          <SettingsGroup title="Gefahrenzone">
+            <SettingsItem
+              icon={trashOutline}
+              label="Alle Daten zurücksetzen"
+              onClick={onReset}
+              color="danger"
+            />
+          </SettingsGroup>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
   );
 };
 
 export default AdvancedTab;
+ 
