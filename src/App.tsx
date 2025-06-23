c79aff9b8f5e6e3eeb1079493b12f23999af5bb4
Gabrielelogiurato <gabriele.logiurato@lernende.bbw.ch>
2025-06-23 11:25:07 +0200

    fix: enable animated routing transitions and iOS-style navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './ionic';
 
 import React from 'react';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { AppRouter } from '@/AppRouter';
 
 const queryClient = new QueryClient();
+
+setupIonicReact({
+  mode: 'ios',
+  animated: true,
+  swipeBackEnabled: true,
+});
 
 function App() {
   return (
@@ -16,7 +21,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <IonApp>
           <IonReactRouter>
-            <IonRouterOutlet id="main">
+            <IonRouterOutlet id="main" animated={true}>
               <Route path="/main" render={() => <AppRouter />} />
               <Route exact path="/" render={() => <Redirect to="/main/" />} />
             </IonRouterOutlet>
