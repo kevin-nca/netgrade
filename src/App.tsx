91f62bc08fccc83cab8e73dc2620fa415ea405e4
Gabrielelogiurato <gabriele.logiurato@lernende.bbw.ch>
2025-07-15 14:27:09 +0200

    feat(routing): fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './ionic';
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { AppRouter } from '@/AppRouter';
@@ -12,9 +14,14 @@ function App() {
   return (
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
+        <IonApp>
+          <IonReactRouter>
+            <IonRouterOutlet id="main">
+              <Route path="/main" render={() => <AppRouter />} />
+              <Route exact path="/" render={() => <Redirect to="/main/" />} />
+            </IonRouterOutlet>
+          </IonReactRouter>
+        </IonApp>
       </QueryClientProvider>
     </React.StrictMode>
   );
