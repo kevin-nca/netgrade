1437166ce940f69894d11ca0bcfbcd1f9c443658
zduram <zduram@bbcag.ch>
2025-08-18 10:55:20 +0200

    fix: update header text on MihalPage to improve clarity

--- src/pages/mihalpage.tsx
+++ src/pages/mihalpage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const MihalPage: React.FC = () => {
 	return (
 		<div style={{ padding: "2rem", textAlign: "center" }}>
-			<h1>Mihal Seite</h1>
+			<h1>Mihal homepage</h1>
 			<p>Willkommen auf der Mihal-Seite! Hier kannst du deinen individuellen Inhalt hinzufügen.</p>
 			<img src="/public/icon.png" alt="Mihal Icon" style={{ width: "120px", marginTop: "2rem" }} />
 		</div>
