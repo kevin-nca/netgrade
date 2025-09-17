63d47a06a474dc904a0ea04730cad38e693d7e0c
MihalDuraki <mihalduraki13@gmail.com>
2025-09-17 15:50:27 +0200

    fix: simplify by removing extras

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,9 +1,9 @@
 describe('Onboarding Flow', () => {
   beforeEach(() => {
-    cy.visit('/main/onboarding');
+    cy.visit('main/onboarding');
   });
 
   it('should complete the onboarding process successfully', () => {
-    cy.get('.welcome-button', { timeout: 10000 }).should('be.visible').click();
+    cy.get('.welcome-button').click();
   });
 });
