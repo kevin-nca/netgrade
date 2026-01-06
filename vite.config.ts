9572e664f10c5cc41643c0dcf2f269bf192c2902
MihalDuraki <mihalduraki13@gmail.com>
2026-01-06 14:52:02 +0100

    Revert "refactor: update module resolution to 'bundler' and enhance App compo…"
    This reverts commit 0f2aee3d7ef82960b2a6528df46b0243f883d57b.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import * as path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [wasm(), react(), legacy()],
   resolve: {
