ef5bb1c3bf600e91edd595387fd449bf0830d22d
Florent Gashi <>
2026-02-03 15:46:48 +0100

    feat: changed string to date in Semester Entity and added Temporal transformer for semester dates

--- src/db/entities/Semester.ts
+++ src/db/entities/Semester.ts
@@ -1,16 +1,36 @@
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, ValueTransformer } from 'typeorm';
+import { Temporal } from '@js-temporal/polyfill';
 import { BaseEntity } from './BaseEntity';
 import { Subject } from './Subject';
+
+const TemporalDateTransformer: ValueTransformer = {
+  to(value: Temporal.PlainDate): Date {
+    return new Date(value.toString());
+  },
+
+  from(value: Date | string): string {
+    if (value instanceof Date) {
+      return value.toISOString().split('T')[0];
+    }
+    return value;
+  },
+};
 
 @Entity('semester')
 export class Semester extends BaseEntity {
   @Column({ type: 'varchar' })
   name!: string;
 
-  @Column({ type: 'text' })
+  @Column({
+    type: 'date',
+    transformer: TemporalDateTransformer,
+  })
   startDate!: string;
 
-  @Column({ type: 'text' })
+  @Column({
+    type: 'date',
+    transformer: TemporalDateTransformer,
+  })
   endDate!: string;
 
   @OneToMany(() => Subject, (subject) => subject.semester)
--- src/db/migrations/1737400000000-add-semester.ts
+++ src/db/migrations/1737400000000-add-semester.ts
@@ -16,8 +16,8 @@ export class AddSemester1737400000000 {
                                 "version"       integer NOT NULL DEFAULT 1,
                                 "appInstanceId" varchar(255),
                                 "name"          varchar NOT NULL,
-                                "startDate"     text NOT NULL,
-                                "endDate"       text NOT NULL
+                                "startDate"     date NOT NULL,
+                                "endDate"       date NOT NULL
       )
     `);
 
