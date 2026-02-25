5e90593b4e563376aedc36974b68076a3b2de8dc
MihalDuraki <mihalduraki13@gmail.com>
2026-02-25 13:49:06 +0100

    refactor: update entity relationships to use Semester instead of Subject in School

--- src/db/entities/Semester.ts
+++ src/db/entities/Semester.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
 import { BaseEntity } from './BaseEntity';
+import { School } from './School';
 import { Subject } from './Subject';
 
 @Entity('semester')
@@ -12,6 +13,16 @@ export class Semester extends BaseEntity {
 
   @Column({ type: 'date' })
   endDate!: Date;
+
+  @Column({ type: 'uuid' })
+  schoolId!: string;
+
+  @ManyToOne(() => School, (school) => school.semesters, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'schoolId' })
+  school!: School;
 
   @OneToMany(() => Subject, (subject) => subject.semester)
   subjects!: Subject[];
