f63a4b401734078c70bb6f0708dc75592b821043
florent-gashi1 <lentig89@gmail.com>
2026-03-31 13:38:08 +0200

    refactor: refactor SchoolCard and EmptySchoolCard into seperate components

---
+++ src/pages/home/settings/components/EmptySchoolCard.tsx
@@ -0,0 +1,22 @@
+import { IonIcon } from '@ionic/react';
+import { schoolOutline } from 'ionicons/icons';
+
+const EmptySchoolCard = () => {
+  return (
+    <div className="settings-item glass-card empty-item">
+      <div className="item-content">
+        <div className="item-icon empty">
+          <IonIcon icon={schoolOutline} />
+        </div>
+        <div className="item-text">
+          <h3 className="item-title">Keine Schulen</h3>
+          <p className="item-subtitle">
+            Tippe auf + um eine Schule hinzuzufügen
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmptySchoolCard;
